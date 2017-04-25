13afa8462ddcc1d8d0e436b27ada92805903cf1d
feiyu <1119290647@qq.com>
2017-04-25 10:20:56 +0800

    文件路径移动

--- src/main/java/org/mybatis/generator/extend/MyTestRun.java
+++ src/main/java/org/mybatis/generator/extend/MyTestRun.java
@@ -4,7 +4,7 @@ import org.mybatis.generator.api.ShellRunner;
 
 public class MyTestRun {
 	public static void main(String[] args) {
-		String config = MyTestRun.class.getClassLoader().getResource("org/generatorConfig.xml").toString();
+		String config = MyTestRun.class.getClassLoader().getResource("generatorConfig.xml").toString();
 		System.out.println(config);
 		System.out.println("a");
 		String[] arg = {"-configfile", config.substring(6), "-overwrite"};
